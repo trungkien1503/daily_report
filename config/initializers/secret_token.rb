a5bea8bb5b2b471cb6d816dc5d3d67990deb9d53
ngtnah <nguyentuananh@nguyentuananh.com>
2013-06-05 09:17:01 +0700

    update gemfile + postgresql database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyReportSystem::Application.config.secret_token = '78a674f0706836e630013f6acf446d642f04f5ae09cd02274710a92fead0dea86d9c42813bbf4bd3a3dea31abf14084915ba1571ab856931b3e555eed2581f14'
